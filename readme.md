c6aa6805566b1f5257b81b9a55c850b20f531567
Amel-CYDF <taehoon1310@korea.ac.kr>
2019-12-27 19:35:40 +0900

    12/27 HW Done

--- readme.md
+++ readme.md
@@ -10,10 +10,18 @@
 * d
 ![Alt text](/191224_ScreenShot/d.png)
 ### 12월 25일
-*text/plain 보내기
+* text/plain 보내기
 ![Alt text](/191225_ScreenShot/text_plain.png)
-*text/plain 보내서 image 받기
+* text/plain 보내서 image 받기
 ![Alt text](/191225_ScreenShot/image.png)
-*text/html 보내기
+* text/html 보내기
 ![Alt text](/191225_ScreenShot/text_html.png)
 ### 12월 26일
+![Alt text](/191226_ScreenShot/cookie.png)
+### 12월 27일
+* 추가: id가 amel 이고 pw가 amel 이면 name을 amel로 갖는 cookie를 생성
+![Alt text](/191227_ScreenShot/idpw.png)
+* 추가: req의 cookie의 name값이 amel이면 403을 return 하면서 화면에 forbidden 출력
+![Alt text](/191227_ScreenShot/send403.png)
+* 스크린샷
+![Alt text](/191226_ScreenShot/403Forbidden.png)
--- routes/login.js
+++ routes/login.js
@@ -11,10 +11,17 @@ router.post('/', function(req, res, next) {
       maxAge: 30000   // 30000밀리초 → 30초
     })
   }
+   
+  if(req.body.id === 'amel' && req.body.pw === 'amel') {
+    res.cookie('name', 'amel', {
+      maxAge: 30000
+    })
+  }
+  
   return res.status(302).redirect("/login");
 });
 
-router.post('/logout', function(req, res, next) {
+router.get('/logout', function(req, res, next) { 
     req.cookies.name = undefined;
     return res.status(302).redirect("/login");
 })
@@ -23,8 +30,15 @@ router.get('/isLogin', function(req, res, next) {
   if(req.cookies.name === undefined) {
     return res.status(401).send("unauthorized");
   }
+
+  // todo(12-27): login한 ID로 cookie값을 설정, 
+  // ID가 waas가 아닌경우 403 Forbidden 공지
+  else if(req.cookies.name === 'amel')
+  {
+    return res.status(403).send("Forbidden");
+  }
+
   else {
-    // todo(12-27): login한 ID로 cookie값을 설정, ID가 waas가 아닌경우 403 Forbidden 공지
     return res.status(200).send("waas team");
   }
 })
